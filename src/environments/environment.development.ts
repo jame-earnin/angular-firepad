5cb9de7584703c293ddae209663197d1dabb0fff
jamesiwat-earnin <siwat.kaolueng@earnin.com>
2023-03-19 23:32:06 +0700

    fix environment firebase

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,11 @@
 export const environment = {
   firebase: {
-    projectId: 'angular-firepad-0',
-    appId: '1:361969918363:web:0a27fb31942f01ac26813e',
-    storageBucket: 'angular-firepad-0.appspot.com',
-    apiKey: 'AIzaSyDFjbOs5LklDC7cpaNx7pmBxqD84YBAF8c',
-    authDomain: 'angular-firepad-0.firebaseapp.com',
-    messagingSenderId: '361969918363',
-    measurementId: 'G-NZWWECPMEB',
-  },};
+    apiKey: "AIzaSyDFjbOs5LklDC7cpaNx7pmBxqD84YBAF8c",
+    authDomain: "angular-firepad-0.firebaseapp.com",
+    databaseURL: "https://angular-firepad-0-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "angular-firepad-0",
+    storageBucket: "angular-firepad-0.appspot.com",
+    messagingSenderId: "361969918363",
+    appId: "1:361969918363:web:0a27fb31942f01ac26813e",
+    measurementId: "G-NZWWECPMEB"
+  }};
