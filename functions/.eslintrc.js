edf096adccd1e8d0f893a2ce43db4a2c29ce2ff4
jamesiwat-earnin <siwat.kaolueng@earnin.com>
2023-03-29 03:19:30 +0700

    add auth

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -25,7 +25,6 @@ module.exports = {
     "import",
   ],
   rules: {
-    "quotes": ["error", "single"],
     "import/no-unresolved": 0,
     "indent": ["error", 2]
   },
--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,5 +1,8 @@
-import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { GithubAuthProvider } from '@angular/fire/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { combineLatest, Subject } from 'rxjs';
 import { fromMonacoWithFirebase } from '../firepad-x';
 
 @Component({
@@ -8,25 +11,60 @@ import { fromMonacoWithFirebase } from '../firepad-x';
   styleUrls: ['./editor.component.scss'],
 })
 export class EditorComponent implements OnInit {
-
   private database: AngularFireDatabase = inject(AngularFireDatabase);
+  auth: AngularFireAuth = inject(AngularFireAuth);
   editorOptions = { 
     language: "typescript",
     fontSize: 18,
     theme: "vs-dark",
     // @ts-ignore
     trimAutoWhitespace: false
- };
+  };
+  editor: any;
+  isLogin = false;
   code: string = 'function x() {\nconsole.log("Hello world!");\n}';
+  editorSubject = new Subject<any>();
   constructor() {}
-  ngOnInit() {}
+  ngOnInit() {
+    combineLatest([this.editorSubject.asObservable(),this.auth.user]).subscribe(([editor,user]) => {
+      if (user?.uid) {
+        const firepad = fromMonacoWithFirebase(this.database.database.ref(), editor, {
+          userId: user.uid,
+          userName: user.displayName! ,
+          userColor: '#FFA611', // Firebase Color
+        });
+  
+        window["firepad"] = firepad;
+        window["editor"] = editor; 
+      }
+    })
+  }
   onInit(editor: any) {
-      const firepad = fromMonacoWithFirebase(this.database.database.ref(), editor, {
-        userId: crypto.randomUUID(),
-        userColor: '#FFA611', // Firebase Color
-      });
-
-      window["firepad"] = firepad;
-      window["editor"] = editor; 
+    this.editor = editor;
+    this.editorSubject.next(editor);
+  }
+  loginViaGithub() {
+    const provider = new GithubAuthProvider();
+    this.auth.signInWithPopup(provider).then((result) => {
+      const credential = result.credential;
+  
+      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+      const token = credential?.providerId;
+  
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available in result.additionalUserInfo.profile.
+      this.isLogin = true;
+        // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      const credential = error.credential;
+      // ...
+    });
   }
 }
