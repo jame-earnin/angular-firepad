44cf551f596debd5c6736c3b2e7b22fbe8e30d75
jamesiwat-earnin <siwat.kaolueng@earnin.com>
2023-03-31 22:43:51 +0700

    remove forked ngx-monaco-editor

--- projects/ngx-monaco-editor/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of ngx-monaco-editor
- */
-
-export * from './lib/editor.component';
-export * from './lib/diff-editor.component';
-export * from './lib/editor.module';
-export * from './lib/config';
-export * from './lib/types';
